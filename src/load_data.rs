96662a664b1ba5a69cb679d16fdfde73778799b4
Seigenkousya <seigenkousya@outlook.jp>
2021-11-06 23:19:02 +0900

    [add] add src/ from prototype

---
+++ src/load_data.rs
@@ -0,0 +1,73 @@
+use std::fs::{self};
+use yew::{html, Html};
+use toml::value::Datetime;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Clock {
+    date: Datetime,
+    remain: Datetime,
+    event: String,
+    description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Timeline {
+    clock: Vec<Clock>,
+}
+
+impl Clock {
+    pub fn html_row(&self) -> Html {
+        html! {
+            <tr>
+                <td> { self.date.clone() } </td>
+                <td> { self.remain.clone() } </td>
+                <td> { self.event.clone() } </td>
+                <td> { self.description.clone() } </td>
+            </tr>
+        }
+    }
+}
+
+impl Timeline {
+    pub fn create_html_table(&self) -> Html {
+        let clocks = 
+            self.clock
+                .iter()
+                .map(|clk: &Clock| clk.html_row());
+
+        html! {
+            <table>
+                { for clocks }
+            </table>
+        }
+    }
+}
+
+pub fn read_toml() -> Result<Timeline, Box<dyn std::error::Error>> {
+    //let raw_data: String = fs::read_to_string("timeline.toml")?;
+    let raw_data: String = "\
+[[clock]]
+date = 2019-03-19
+remain = 00:00:45
+event = 'abcdefg'
+description = \"\"\"\\
+    god is dead.\
+    \"\"\"
+
+[[clock]]
+date = 2021-08-19
+remain = 00:00:45
+event = 'hijklmn'
+description = \"\"\"\\
+    It's a resurrection day.\\
+    \"\"\"
+".to_string();
+
+    let parsed_toml: Result<Timeline, toml::de::Error> = toml::from_str(&raw_data);
+    let timeline = match parsed_toml {
+        Ok(tl) => tl,
+        Err(msg) => panic!("Filed to parse TOML: {}", msg),
+    };
+    Ok(timeline)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,80 @@
+extern crate serde;
+extern crate toml;
+mod load_data;
+
+use yew::prelude::*;
+
+
+enum Msg {
+    AddOne,
+    MinusOne,
+}
+
+struct Model {
+    // `ComponentLink` is like a reference to a component.
+    // It can be used to send messages to the component
+    link: ComponentLink<Self>,
+    value: i64,
+    clock_data: load_data::Timeline,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+            clock_data: load_data::read_toml().unwrap(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            },
+            Msg::MinusOne => {
+                self.value -= 1;
+                true
+            },
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        let clocks = self.clock_data.create_html_table();
+        html! {
+            <>
+                <main>
+                    <div>
+                        <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                        <button onclick=self.link.callback(|_| Msg::MinusOne)>{ "-1" }</button>
+                        <p>{ self.value }</p>
+                    </div>
+                    <div>{ clocks }</div>
+                </main>
+                <footer class="footer">
+                    <div>
+                        { "copyright 2021- XXXXX: " }
+                        <a href="https://yew.rs">{ "Yew" }</a><br/>
+                        { "contribute: visit " }
+                        <a href="https://github.com/Alignof/yew_practice">{ "https://github.com/Alignof/yew_practice" }</a>
+                    </div>
+                </footer>
+            </>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
